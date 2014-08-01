7b9fa6be70e2c638f21398ddb83337336f98d6f5
Michael Zamani <michael.zamani1@gmail.com>
2014-08-01 19:05:11 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,11 +10,22 @@
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
-    // put update code here
+    // Run every frame.
+    // delta is the time that has elapsed since the last time this was run. Usually 1/60.
+    timeSinceObstacle += delta;
+    if (timeSinceObstacle > 2.0f) {
+        // Add a new obstacle.
+        [self addObstacle];
+        
+        // Reset the timer.
+        timeSinceObstacle = 0.0f;
+    }
+    
 }
 
 // put new methods here
